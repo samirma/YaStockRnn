7adb304ac5091b2869ee889d28d7cfaeadb25bdc
Samir Antonio <samir.antonio@thetrainline.com>
2019-09-02 14:48:05 +0100

    feat remove feautres

--- state_util.py
+++ state_util.py
@@ -1,114 +1,118 @@
-last_price = 0
-last_time = 0
-should_buy = 0
-should_sell = 0
-
 from datetime import datetime
 
-# integer encode input data
-def onehot_encoded (integer_encoded, char_to_int = 2):
-    # one hot encode
-    onehot_encoded = list()
-    letter = [0 for _ in range(char_to_int)]
-    letter[integer_encoded] = 1
-    onehot_encoded.append(letter)
+class StateUtil():
+    
+    def __init__(self):
+        self.should_buy = 0
+        self.should_sell = 0
+        self.TIMESTAMP_KEY = "timestamp"
+        self.ASKS_KEY = "asks"
+        self.BIDS_KEY = "bids"
+        self.PRICE_KEY = "price"
+        self.AMOUNT_KEY = "amount"
+    
+    # integer encode input data
+    def onehot_encoded (self, integer_encoded, char_to_int = 2):
+        # one hot encode
+        onehot_encoded = list()
+        letter = [0 for _ in range(char_to_int)]
+        letter[integer_encoded] = 1
+        onehot_encoded.append(letter)
 
-    return onehot_encoded[0]
+        return onehot_encoded[0]
 
-def get_date(state):
-    timestamp = int(state["timestamp"])
-    return datetime.fromtimestamp(timestamp)
+    def get_date(self, state):
+        timestamp = int(state[self.TIMESTAMP_KEY])
+        return datetime.fromtimestamp(timestamp)
 
 
-def get_parse_state(raw_state, last_price, last_time):
-    list = []
-    price = raw_state["price"]
+    def get_parse_state(self, raw_state):
+        list = []
+        price = raw_state[self.PRICE_KEY]
+
+        list.append(raw_state[self.AMOUNT_KEY])
+
+        def prepare_orders(orders, price, multi):
+            amount = float(orders[0][1])
+            for order in orders:
+                list.append((float(order[0])/price) * multi)
+                list.append(float(order[1])/amount)
+
+        history_step = 5
+        bids = raw_state[self.BIDS_KEY][:history_step]
+        asks = raw_state[self.ASKS_KEY][:history_step]
+        prepare_orders(bids, price, 1)
+        prepare_orders(asks, price, -1)
+
+        #if last_price != 0:
+        #    list.extend([price/last_price])
+        #else:
+        #    list.extend([0])
+
+        #current_timestamp = int(raw_state[self.TIMESTAMP_KEY])
+        #if last_time != 0:
+        #    list.extend([current_timestamp / last_time])
+        #else:
+        #    list.extend([0])
+        #list = []
+        #list.extend([raw_state['timestamp']])
+        #print(current_timestamp)
+        return list
+
+    def get_future_state(self, data_gen, sec=120): # 2 min in future
+        state_timestamp = int(data_gen.get_from_index(data_gen.index-1)[self.TIMESTAMP_KEY])
         
-    list.append(raw_state["amount"])
+        timestamp_limit = state_timestamp + sec
+        #print("Current timestamp", state_timestamp, " ==== ", timestamp_limit)
+        index = 0
+        timestamp = 0
+        while timestamp < timestamp_limit:
+            state = data_gen.get_json_from_timestamp(timestamp_limit + index)
+            if (state):
+                timestamp_found = int(state[self.TIMESTAMP_KEY])
+                #print("Current timestamp", state_timestamp, " ==== ", timestamp_found)
+                return state
+            index += 1
+        return False
 
-    def prepare_orders(orders, price, multi):
-        amount = float(orders[0][1])
-        for order in orders:
-            list.append((float(order[0])/price) * multi)
-            list.append(float(order[1])/amount)
-
-    history_step = 5
-    bids = raw_state["bids"][:history_step]
-    asks = raw_state["asks"][:history_step]
-    prepare_orders(bids, price, 1)
-    prepare_orders(asks, price, -1)
-
-    if last_price != 0:
-        list.extend([price/last_price])
-    else:
-        list.extend([0])
-        
-    current_timestamp = int(raw_state['timestamp'])
-    if last_time != 0:
-        list.extend([current_timestamp / last_time])
-    else:
-        list.extend([0])
-    #list = []
-    #list.extend([raw_state['timestamp']])
-    #print(current_timestamp)
-    return list
-
-def get_future_state(data_gen, sec=120): # 2 min in future
-    state_timestamp = int(data_gen.get_from_index(data_gen.index-1)['timestamp'])
-    timestamp_limit = state_timestamp + sec
-    #print("Current timestamp", state_timestamp, " ==== ", timestamp_limit)
-    index = 0
-    timestamp = 0
-    while timestamp < timestamp_limit:
-        index += 3
-        state = data_gen.get_from_index(data_gen.index + index)
-        timestamp = int(state['timestamp'])
-    return state
-
-
-
-def get_state(raw_state, data_gen):
-    global last_price
-    global last_time
-    global should_buy
-    global should_sell
     
-    list = get_parse_state(raw_state, last_price, last_time)
+    def get_bid_goal(self, ask):
+        return (ask * 1.0001)
 
-    furure_state = get_future_state(data_gen)
-    future_price = furure_state["price"]
-    
-    current_price = raw_state["price"]
-    current_timestamp = int(raw_state['timestamp'])
-    
-    current_bid = float(raw_state["bids"][0][0])
-    future_bid = float(furure_state["bids"][0][0])
+    def get_state(self, raw_state, data_gen):
 
-    will_offer_less = (future_bid/current_bid) < 0.99998
+        list = self.get_parse_state(raw_state)
 
-    ask = float(raw_state["asks"][0][0]) 
-    predicted = (ask * 1.0002)
-    is_value_incresed = future_bid >= predicted
+        furure_state = self.get_future_state(data_gen)
+        future_price = furure_state[self.PRICE_KEY]
 
-    if is_value_incresed:
-        should_buy += 1
-        #print (get_date(raw_state), ": ", ask, " ==== ", predicted, " ===== ", get_date(furure_state), " ===== ", future_bid)
-        #print(raw_state)
-        #print(furure_state)
-        #print("=====")
-        y = onehot_encoded(0)
-    #elif will_offer_less:
-        #print (get_date(raw_state), ": ", current_bid, " #### ", (current_price + 0.2), " #### ", future_bid)
-    #    should_sell += 1
-    #    y = onehot_encoded(2)
-    else:
-        #print (current_price, " ==== ", (current_price + 0.2), " ===== ", furure_state)
-        should_sell += 1
-        y = onehot_encoded(1)
-           
-    #print (y)
-    #print (get_date(raw_state), " ==== ", get_date(furure_state))
-    
-    last_price = current_price
-    last_time = current_timestamp
-    return [list, y]
+        current_price = raw_state[self.PRICE_KEY]
+        current_timestamp = int(raw_state[self.TIMESTAMP_KEY])
+
+        current_bid = float(raw_state[self.BIDS_KEY][0][0])
+        future_bid = float(furure_state[self.BIDS_KEY][0][0])
+
+        will_offer_less = (future_bid/current_bid) < 0.99998
+
+        ask = float(raw_state[self.ASKS_KEY][0][0]) 
+        predicted = self.get_bid_goal(ask)
+        is_value_incresed = future_bid >= predicted
+
+        if is_value_incresed:
+            self.should_buy += 1
+            print (self.get_date(raw_state), ": ", ask, " ==== ", predicted, " ===== ", self.get_date(furure_state), " ===== ", future_bid)
+            #print(raw_state)
+            #print(furure_state)
+            #print("=====")
+            y = self.onehot_encoded(0)
+        else:
+            #print (current_price, " ==== ", (current_price + 0.2), " ===== ", furure_state)
+            self.should_sell += 1
+            y = self.onehot_encoded(1)
+
+        #print (y)
+        #print (get_date(raw_state), " ==== ", get_date(furure_state))
+
+        last_price = current_price
+        last_time = current_timestamp
+        return [list, y]
